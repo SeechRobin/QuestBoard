1ce5976141348e54f98893d10168532ee4b6d1f4
Robin Mukanganise <tinashe.dev@gmail.com>
2014-04-26 12:36:35 +0200

    Initial Commit

---
+++ QuestBoard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuestBoard
+//
+//  Created by Robin Mukanganise on 2014/04/26.
+//  Copyright (c) 2014 Robin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ QuestBoard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuestBoard
+//
+//  Created by Robin Mukanganise on 2014/04/26.
+//  Copyright (c) 2014 Robin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
